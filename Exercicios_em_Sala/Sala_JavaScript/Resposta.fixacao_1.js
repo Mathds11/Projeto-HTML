9c635d4b7346ac8b5dcfa2c3b0911215520fccb1
Matheus <matheus.da.silvajk@gmail.com>
2023-06-06 22:20:58 -0400

    js

---
+++ Exercicios_em_Sala/Sala_JavaScript/Resposta.fixacao_1.js
@@ -0,0 +1,8 @@
+let valor = document.getElementsById('valor');
+let resultado = document.getElementsById('resultado');
+
+valor.onchange = function(){
+    valor.value = valor * 3;
+    resultado.value
+};
+
