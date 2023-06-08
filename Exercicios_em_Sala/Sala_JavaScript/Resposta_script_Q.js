d8023db18bbbdbe13ee871c553e759cecc1b7118
Matheus <matheus.da.silvajk@gmail.com>
2023-06-08 10:56:50 -0400

    respost

---
+++ Exercicios_em_Sala/Sala_JavaScript/Resposta_script_Q.js
@@ -0,0 +1,47 @@
+/* Resp Questionario 1
+let n = document.querySelector('#valor');
+let botao = document.querySelector('#bt')
+let resultado = document.querySelector('#r');
+
+function btFuncao(){
+    if(parseFloat(n.value) >= 20 && parseFloat(n.value) <= 90){
+        resultado.innerHTML = 'Valor está entre 20 e 90';
+    }else{
+        resultado.innerHTML = 'Valor não está entre 20 e 90';
+    }
+};
+*/
+/* Resp Questionario 2
+let n = document.querySelector('#valor');
+let botao = document.querySelector('#bt')
+let resultado = document.querySelector('#r');
+
+function btFuncao(){
+    if(parseFloat(n.value) > 20){
+        resultado.innerHTML = n.value/2;
+    };
+};
+*/
+/* Resp Questionario 3
+uf.onchange = function(){
+    switch(this.value){
+            case 'RJ' : natural.innerHTML = 'Carioca'; break;
+            case 'SP' : natural.innerHTML = 'Paulista'; break;
+            case 'MG' : natural.innerHTML = 'Mineiro'; break;
+            default : natural.innerHTML = 'Outros estados';
+    }
+};
+*/
+/* Resp Questionario 4
+let n = document.querySelector('#valor');
+let botao = document.querySelector('#bt')
+let resultado = document.querySelector('#r');
+
+function btFuncao(){
+    if(parseFloat(n.value)%5 > 0){
+        resultado.innerHTML = 'Não é divisivel por 5';
+    }else{
+        resultado.innerHTML = 'É divisivel por 5';
+    }
+};
+*/
