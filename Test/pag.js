9cb959c29b878d779742b993018bd42f0f95bb36
Matheus <matheus.da.silvajk@gmail.com>
2023-06-09 16:28:04 -0400

    test

---
+++ Test/pag.js
@@ -0,0 +1,19 @@
+/*
+let texto = document.getElementById('txtinfo');
+let r = document.getElementById('resultado');
+
+texto.onchange = function(){
+    r.value = texto.value ** 3;
+    r.innerHTML = 'Valor do cubo é: ' + r.value;
+}
+*/
+let valor1 = document.getElementById('valor1');
+let valor2 = document.getElementById('valor2');
+let valor3 = document.getElementById('valor3');
+let texto = document.getElementById('texto');
+
+function btFuncao(){
+    let i = 0;
+    i = Math.max(valor1.value , valor2.value , valor3.value);
+    texto.innerHTML = 'Maior valor é: ' + i;
+}
