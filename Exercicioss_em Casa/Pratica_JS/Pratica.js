945c94bed8363550a6f6ba54ca063a2e98191c63
Matheus <matheus.da.silvajk@gmail.com>
2023-06-18 12:00:57 -0400

    ajuste

--- Exercicioss_em Casa/Pratica_JS/Pratica.js
+++ Exercicioss_em Casa/Pratica_JS/Pratica.js
@@ -7,21 +7,21 @@ let consumo = document.getElementById('consumo');
 function btCalculo(){
     if(carro.value == 'A'){
         if(validar.checked == true){
-            consumo.value = (8 * km.value)*1.05;
+            consumo.value = (km.value / 8)*1.05;
         }else{
-            consumo.value = 8 * km.value;
+            consumo.value = km.value / 8;
         };        
     }if(carro.value == 'B'){
         if(validar.checked == true){
-            consumo.value = (9 * km.value)*1.05;
+            consumo.value = (km.value / 9)*1.05;
         }else{
-            consumo.value = 9 * km.value;
+            consumo.value = km.value / 9;
         };        
     }if(carro.value == 'C'){
         if(validar.checked == true){
-            consumo.value = (12 * km.value)*1.05;
+            consumo.value = (km.value / 12)*1.05;
         }else{
-            consumo.value = 12 * km.value;
+            consumo.value = km.value / 12;
         };        
     };
     consumo.innerHTML = 'Quantidade consumida: <br>' + consumo.value.toFixed(2) + ' Litros';
